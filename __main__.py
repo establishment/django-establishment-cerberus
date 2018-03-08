1ddd0232c95701c9cd07c0acfbb59420f289bbce
links234 <murtaza_alexandru73@yahoo.com>
2018-03-08 05:27:49 -0800

    Add support for detached establishment module

--- __main__.py
+++ __main__.py
@@ -7,6 +7,20 @@ patch_psycopg()
 import os
 import sys
 
+establishment_path = None
+new_sys_argv = []
+for arg in sys.argv:
+    if arg.startswith("--establishment-path="):
+        establishment_path = arg[len("--establishment-path="):]
+    else:
+        new_sys_argv.append(arg)
+
+sys.argv.clear()
+sys.argv += new_sys_argv
+
+if establishment_path:
+    sys.path.insert(1, establishment_path)
+
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(1, parent_dir)
 
